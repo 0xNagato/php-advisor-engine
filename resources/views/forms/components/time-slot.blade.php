cffab74bf5fd625fdae977df38045cb918b9d7bd
Andrew Weir <andru.weir@gmail.com>
2024-02-15 13:34:52 -0500

    TimeSlot component

---
+++ resources/views/forms/components/time-slot.blade.php
@@ -0,0 +1,27 @@
+<x-dynamic-component
+    :component="$getFieldWrapperView()"
+    :field="$field"
+>
+    <!--suppress JSUnresolvedReference, BadExpressionStatementJS -->
+    <div
+        x-data="{ state: $wire.$entangle('{{ $getStatePath() }}'), formatTime(time) {
+            let [hour, minute] = time.split(':');
+            let date = new Date();
+            date.setHours(hour, minute);
+            return date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+        } }"
+        class="flex items-center space-x-4 text-sm"
+    >
+
+
+        <label x-text="formatTime(state.start_time)" class="font-semibold" for="{{ $getStatePath() }}">
+        </label>
+        <x-filament::input.checkbox x-model="state.is_available" id="{{ $getStatePath() }}"/>
+        <x-filament::input.wrapper prefix-icon="heroicon-s-user-circle" class="flex-grow">
+            <x-filament::input
+                type="number"
+                x-model="state.available_tables"
+            />
+        </x-filament::input.wrapper>
+    </div>
+</x-dynamic-component>
