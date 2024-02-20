7560bcf2f6fc248516c15c0745cb1ba367e0621a
Andrew Weir <andru.weir@gmail.com>
2024-02-20 06:12:51 -0500

    Adding headings

--- app/Filament/Resources/ConciergeResource/Pages/ViewConcierge.php
+++ app/Filament/Resources/ConciergeResource/Pages/ViewConcierge.php
@@ -7,12 +7,23 @@ use App\Filament\Widgets\ConciergeStatsOverview;
 use App\Filament\Widgets\RecentBookings;
 use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
+use Illuminate\Contracts\Support\Htmlable;
 use STS\FilamentImpersonate\Pages\Actions\Impersonate;
 
 class ViewConcierge extends ViewRecord
 {
     protected static string $resource = ConciergeResource::class;
     protected static string $view = 'filament.resources.concierges.pages.view-concierge';
+
+    public function getHeading(): string
+    {
+        return $this->getRecord()->user->name;
+    }
+
+    public function getSubheading(): string|Htmlable|null
+    {
+        return $this->getRecord()->hotel_name;
+    }
 
     protected function getHeaderActions(): array
     {
--- app/Filament/Resources/RestaurantResource/Pages/ViewRestaurant.php
+++ app/Filament/Resources/RestaurantResource/Pages/ViewRestaurant.php
@@ -7,6 +7,7 @@ use App\Filament\Widgets\RecentBookings;
 use App\Filament\Widgets\RestaurantStatsOverview;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
+use Illuminate\Contracts\Support\Htmlable;
 use STS\FilamentImpersonate\Pages\Actions\Impersonate;
 
 class ViewRestaurant extends ViewRecord
@@ -14,6 +15,15 @@ class ViewRestaurant extends ViewRecord
     protected static string $resource = RestaurantResource::class;
     protected static string $view = 'filament.resources.restaurants.pages.view-restaurant';
 
+    public function getHeading(): string|Htmlable
+    {
+        return $this->getRecord()->restaurant_name;
+    }
+
+    public function getSubheading(): string|Htmlable|null
+    {
+        return $this->getRecord()->user->name;
+    }
 
     protected function getHeaderActions(): array
     {
