f366390742f5c12f34f04c31185590e91f8067da
Andrew Weir <andru.weir@gmail.com>
2024-02-23 15:13:29 -0500

    Fix format

--- resources/views/forms/components/time-slot.blade.php
+++ resources/views/forms/components/time-slot.blade.php
@@ -1,27 +1,19 @@
-<x-dynamic-component
-    :component="$getFieldWrapperView()"
-    :field="$field"
->
+<x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
     <!--suppress JSUnresolvedReference, BadExpressionStatementJS -->
-    <div
-        x-data="{ state: $wire.$entangle('{{ $getStatePath() }}'), formatTime(time) {
-            let [hour, minute] = time.split(':');
-            let date = new Date();
-            date.setHours(hour, minute);
-            return date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
-        } }"
-        class="flex items-center space-x-4 text-xs"
-    >
+    <div x-data="{
+        state: $wire.$entangle('{{ $getStatePath() }}'),
+        formatTime(time) {
+            const date = new Date(time);
+            return date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true, timeZone: 'UTC' });
+        }
+    }" class="flex items-center space-x-4 text-xs">
 
 
-        <label x-text="formatTime(state.start_time)" class="font-semibold flex-grow" for="{{ $getStatePath() }}">
+        <label x-text="formatTime(state.start_time)" class="font-semibold" for="{{ $getStatePath() }}">
         </label>
-        <x-filament::input.checkbox x-model="state.is_available" id="{{ $getStatePath() }}"/>
+        <x-filament::input.checkbox x-model="state.is_available" id="{{ $getStatePath() }}" />
         <x-filament::input.wrapper prefix-icon="heroicon-s-user-circle" class="flex-grow">
-            <x-filament::input
-                type="number"
-                x-model="state.available_tables"
-            />
+            <x-filament::input type="number" x-model="state.available_tables" />
         </x-filament::input.wrapper>
     </div>
 </x-dynamic-component>
