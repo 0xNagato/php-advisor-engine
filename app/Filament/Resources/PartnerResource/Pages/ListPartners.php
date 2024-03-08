688d540c656a983e0e2510fd950d22d7c553256f
Andrew Weir <andru.weir@gmail.com>
2024-03-07 22:07:31 -0500

    Yaaa

--- app/Filament/Resources/PartnerResource/Pages/ListPartners.php
+++ app/Filament/Resources/PartnerResource/Pages/ListPartners.php
@@ -4,16 +4,43 @@ namespace App\Filament\Resources\PartnerResource\Pages;
 
 use App\Filament\Resources\PartnerResource;
 use Filament\Actions;
+use Filament\Forms\Components\DatePicker;
+use Filament\Pages\Dashboard\Actions\FilterAction;
+use Filament\Pages\Dashboard\Concerns\HasFiltersAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListPartners extends ListRecords
 {
+    use HasFiltersAction;
+
     protected static string $resource = PartnerResource::class;
 
+    // protected static string $view = 'filament.resources.concierges.pages.view-concierge';
+
+
+    public function mount(): void
+    {
+        $this->filters = [
+            'startDate' => $this->filters['startDate'] ?? now()->subDays(30),
+            'endDate' => $this->filters['endDate'] ?? now(),
+        ];
+    }
+    
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            FilterAction::make()
+                ->label('Date Range')
+                ->iconButton()
+                ->icon('heroicon-o-calendar')
+                ->form([
+                    DatePicker::make('startDate'),
+                    DatePicker::make('endDate'),
+                    // ...
+                ]),
+            Actions\CreateAction::make()
+                ->icon('gmdi-business-center-o')
+                ->iconButton(),
         ];
     }
 }
