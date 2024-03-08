a991891d6524b88268c11065fdb35ab910a4a352
Andrew Weir <andru.weir@gmail.com>
2024-03-07 23:16:13 -0500

    Yup

--- app/Filament/Resources/PartnerResource/Pages/CreatePartner.php
+++ app/Filament/Resources/PartnerResource/Pages/CreatePartner.php
@@ -3,9 +3,80 @@
 namespace App\Filament\Resources\PartnerResource\Pages;
 
 use App\Filament\Resources\PartnerResource;
+use App\Notifications\PartnerCreated;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+use Str;
+use Ysfkaya\FilamentPhoneInput\Forms\PhoneInput;
 
 class CreatePartner extends CreateRecord
 {
     protected static string $resource = PartnerResource::class;
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('User Registration')
+                    ->icon('heroicon-m-user')
+                    ->schema([
+                        TextInput::make('first_name')
+                            ->label('First Name')
+                            ->placeholder('First Name')
+                            ->autocomplete(false)
+                            ->required(),
+                        TextInput::make('last_name')
+                            ->label('Last Name')
+                            ->placeholder('Last Name')
+                            ->autocomplete(false)
+                            ->required(),
+                        TextInput::make('email')
+                            ->label('Email Address')
+                            ->placeholder('name@domain.com')
+                            ->autocomplete(false)
+                            ->email()
+                            ->unique(User::class, 'email')
+                            ->required(),
+                        PhoneInput::make('phone')
+                            ->label('Phone Number')
+                            ->placeholder('Phone Number')
+                            ->hint('Used for SMS notifications')
+                            ->onlyCountries(['US', 'CA'])
+                            ->initialCountry('US')
+                            ->required(),
+                    ]),
+                Section::make('Partner Information')
+                    ->icon('gmdi-business-center-o')
+                    ->schema([
+                        TextInput::make('percentage')
+                            ->label('Percentage')
+                            ->placeholder('Percentage')
+                            ->default(10)
+                            ->suffix('%')
+                            ->numeric()
+                            ->required(),
+                    ]),
+            ]);
+    }
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $user = User::create([
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'email' => $data['email'],
+            'phone' => $data['phone'],
+            'password' => Str::random(8),
+        ]);
+
+        $user->assignRole('partner');
+        $user->notify(new PartnerCreated($user));
+
+        return $user->partner()->create([
+            'percentage' => $data['percentage'],
+        ]);
+    }
 }
---
+++ app/Notifications/PartnerCreated.php
@@ -0,0 +1,79 @@
+Sure, you can set up the `PartnerCreated` notification similar to the `RestaurantCreated` notification. You need to import the necessary classes and define the `via`, `toMail`, `toTwilio`, `toArray`, and `passwordResetUrl` methods. Here's how you can do it:
+
+```php
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Filament\Facades\Filament;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Password;
+use NotificationChannels\Twilio\TwilioChannel;
+use NotificationChannels\Twilio\TwilioMessage;
+use NotificationChannels\Twilio\TwilioSmsMessage;
+
+class PartnerCreated extends Notification
+{
+    use Queueable;
+
+    private string $passwordResetUrl;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public User $user)
+    {
+        $this->passwordResetUrl = $this->passwordResetUrl();
+    }
+
+    protected function passwordResetUrl(): string
+    {
+        $token = Password::createToken($this->user);
+
+        return Filament::getResetPasswordUrl($token, $this->user);
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail', TwilioChannel::class];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage())
+            ->from('info@primavip.co')
+            ->subject('Welcome to the Prima!')
+            ->greeting('Welcome to the Prima!')
+            ->line('You have been invited to the Prima!')
+            ->action('Setup Password', $this->passwordResetUrl)
+            ->line('If you did not expect to receive an invitation to the Prima, you may discard this email.');
+    }
+
+    public function toTwilio(object $notifiable): TwilioSmsMessage|TwilioMessage
+    {
+        return (new TwilioSmsMessage())
+            ->content("Welcome to the Prima! Setup your password at {$this->passwordResetUrl}");
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [];
+    }
+}
+
