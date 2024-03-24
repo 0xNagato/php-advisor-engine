8d63bddcb026c2edeb1e1619133afc98f58a920c
Andrew Weir <andru.weir@gmail.com>
2024-03-24 02:19:17 -0400

    Tester

--- resources/views/livewire/customer-invoice.blade.php
+++ resources/views/livewire/customer-invoice.blade.php
@@ -2,7 +2,7 @@
     <style>
         @media (max-width: 1024px) {
             .invoice-container {
-                height: calc(100vh - 108px);
+                height: calc(100vh - 138px);
             }
         }
     </style>
