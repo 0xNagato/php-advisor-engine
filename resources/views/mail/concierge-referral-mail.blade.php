482faf9ef0d2eeba35152b153ec5a80eaf56d0b1
Andrew Weir <andru.weir@gmail.com>
2024-03-29 14:31:36 -0400

    Update email

--- resources/views/mail/concierge-referral-mail.blade.php
+++ resources/views/mail/concierge-referral-mail.blade.php
@@ -9,17 +9,17 @@ PRIMA was created for two very simple reasons:  To make it easier for consumers 
 
 As a PRIMA Concierge, you will have access to book reservations at the most in-demand restaurants and experiences around the world, reservations that are often not available through traditional channels.  And now, rather than asking for a "favor," you will be compensated for the booking through PRIMA, at scale.
 
-Here's how PRIMA works:
+**Here's how PRIMA works:**
 
 After you create your PRIMA account, you will have access to the PRIMA Reservation Hub as well as the PRIMA Platform, where all of your earnings will be visible in real time.
 
-As a PRIMA Concierge, you will receive:
+**As a PRIMA Concierge, you will receive:**
 
-10% of the gross booking fees for the first 20 monthly reservations.
-12% of the gross booking fees for reservations 20 through 50.
-15% of the gross booking fees for any reservations after the first 50.
+- 10% of the gross booking fees for the first 20 monthly reservations.
+- 12% of the gross booking fees for reservations 20 through 50.
+- 15% of the gross booking fees for any reservations after the first 50.
 
-Example:
+**Example:**
 
 If you are booking your 15th monthly reservation, and the reservation is for 6 people, the total booking fee is $400 and you will earn 12% or $48 for the reservation.
 
@@ -29,11 +29,11 @@ All tiers reset at the end of each calendar month.
 
 In addition, PRIMA offers a two-tier referral structure for concierge referrals, allowing you to earn up to an additional 15% in revenue.
 
-Here's how it works:
+**Here's how it works:**
 
 You can also earn additional income by referring a concierge just like yourself to PRIMA.  PRIMA will pay you 10% of platform fees for any concierge you refer, plus an additional 5% for any concierge they may refer.
 
-Example:
+**Example:**
 
 If a concierge you referred is booking their 15th monthly reservation, and the reservation is for 6 people, the total booking fee is $400.  The restaurant will earn 60% or $240, the concierge will earn 12% or $48 for the reservation.  You will receive 10% of the balance, or ($400 less $240 less $48) * 10% = $11.20
 
@@ -46,5 +46,6 @@ We look forward to working with you and are excited to have you on our team.
 <x-mail::button :url="$passwordResetUrl" color="brand">Secure Your Account</x-mail::button>
 
 Welcome aboard!
+
 Team PRIMA
 </x-mail::message>
