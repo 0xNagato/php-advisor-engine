805841c2adac0ac321f4dfd872037ec029c39077
Andrew Weir <andru.weir@gmail.com>
2024-04-08 15:13:21 -0400

    Resend invitation via SMS

--- app/Livewire/Concierge/ReferralsTable.php
+++ app/Livewire/Concierge/ReferralsTable.php
@@ -2,10 +2,10 @@
 
 namespace App\Livewire\Concierge;
 
+use App\Events\ConciergeReferredViaText;
 use App\Filament\Pages\Concierge\ConciergeReferralEarnings;
 use App\Models\ConciergeReferral;
 use App\Notifications\ConciergeReferredEmail;
-use App\Notifications\ConciergeReferredText;
 use Filament\Notifications\Notification;
 use Filament\Tables\Actions\Action;
 use Filament\Tables\Columns\IconColumn;
@@ -48,8 +48,8 @@ class ReferralsTable extends BaseWidget
                 IconColumn::make('has_secured')
                     ->label('Active')
                     ->alignCenter()
-                    ->icon(fn (string $state): string => empty($state) ? 'heroicon-o-x-circle' : 'heroicon-o-check-circle')
-                    ->color(fn (string $state): string => empty($state) ? 'danger' : 'success'),
+                    ->icon(fn(string $state): string => empty($state) ? 'heroicon-o-x-circle' : 'heroicon-o-check-circle')
+                    ->color(fn(string $state): string => empty($state) ? 'danger' : 'success'),
             ])
             ->actions([
                 Action::make('resendInvitation')
@@ -57,11 +57,11 @@ class ReferralsTable extends BaseWidget
                     ->iconButton()
                     ->color('indigo')
                     ->requiresConfirmation()
-                    ->hidden(fn (ConciergeReferral $record) => $record->has_secured)
+                    ->hidden(fn(ConciergeReferral $record) => $record->has_secured)
                     ->action(function (ConciergeReferral $record) {
-                        if (! blank($record->phone)) {
-                            $record->notify(new ConciergeReferredText($record));
-                        } elseif (! blank($record->email)) {
+                        if (!blank($record->phone)) {
+                            ConciergeReferredViaText::dispatch($record);
+                        } elseif (!blank($record->email)) {
                             $record->notify(new ConciergeReferredEmail($record));
                         }
 
