c40fcecdda2e41376719988c62006c67277f791b
Andrew Weir <andru.weir@gmail.com>
2024-04-08 15:15:10 -0400

    Partner email

--- resources/views/mail/partner-welcome-mail.blade.php
+++ resources/views/mail/partner-welcome-mail.blade.php
@@ -2,11 +2,9 @@
 # Welcome to PRIMA!
 
 We are excited to have you as a member of our team and are looking forward to working with you. In order to sign
-into the Prima system, please click the link below to create your password.
+into the PRIMA system, please click the link below to create your password.
 
 Once you've logged in, ensure that you update your payment info so we know where to send your payments.
-
-You can begin making reservations and earning immediately.
 
 If you have any questions, please do not hesitate to contact us, and we will do our best to get back to you as soon
 as possible.
@@ -14,5 +12,6 @@ as possible.
 <x-mail::button :url="$passwordResetUrl" color="brand">Secure Your Account</x-mail::button>
 
 Sincerely,
+
 Team PRIMA
 </x-mail::message>
