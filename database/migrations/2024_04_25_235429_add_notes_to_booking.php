e9dd22ba3ed70bbfc3abca1ea7a59114b66d90dd
Andrew Weir <andru.weir@gmail.com>
2024-04-25 20:07:48 -0400

    Fix type

--- database/migrations/2024_04_25_235429_add_notes_to_booking.php
+++ database/migrations/2024_04_25_235429_add_notes_to_booking.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::table('booking', function (Blueprint $table) {
+        Schema::table('bookings', function (Blueprint $table) {
             $table->text('notes')->nullable();
         });
     }
@@ -20,7 +20,7 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::table('booking', function (Blueprint $table) {
+        Schema::table('bookings', function (Blueprint $table) {
             $table->dropColumn('notes');
         });
     }
