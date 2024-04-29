f4e49dd330a85768fd8a6ef77b56173122d1cb7c
Andrew Weir <andru.weir@gmail.com>
2024-04-29 11:38:09 -0400

    Fix twilio credentials

--- config/services.php
+++ config/services.php
@@ -48,8 +48,8 @@ return [
     ],
 
     'twilio' => [
-        'sid' => env('TWILIO_SID'),
-        'token' => env('TWILIO_TOKEN'),
+        'sid' => env('TWILIO_ACCOUNT_SID'),
+        'token' => env('TWILIO_AUTH_TOKEN'),
         'from' => env('TWILIO_FROM'),
     ],
 ];
