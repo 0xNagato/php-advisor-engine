baf264e3b2636b0623fae2717985424eeb5fe122
Andrew Weir <andru.weir@gmail.com>
2024-04-30 14:49:46 -0400

    Moved concierge menu back to top left

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -43,7 +43,7 @@ class AppServiceProvider extends ServiceProvider
 
         Filament::registerRenderHook(
             'panels::body.end',
-            static fn(): string => <<<'HTML'
+            static fn (): string => <<<'HTML'
                 <div x-data="" x-init="
                     if (!localStorage.getItem('sidebar_initialized')) {
                         localStorage.setItem('sidebar_initialized', true);
@@ -58,7 +58,7 @@ class AppServiceProvider extends ServiceProvider
          */
         FilamentView::registerRenderHook(
             PanelsRenderHook::HEAD_START,
-            static fn() => new HtmlString("
+            static fn () => new HtmlString("
                 <script>
                 const { userAgent } = window.navigator;
                 if (/PrimaApp/.test(userAgent)) {
@@ -70,12 +70,12 @@ class AppServiceProvider extends ServiceProvider
 
         FilamentView::registerRenderHook(
             PanelsRenderHook::USER_MENU_BEFORE,
-            static fn() => view('filament.admin.logo')
+            static fn () => view('filament.admin.logo')
         );
 
         FilamentView::registerRenderHook(
             PanelsRenderHook::PAGE_END,
-            static fn() => new HtmlString('
+            static fn () => new HtmlString('
             <div class="mb-4 text-xs text-center">
                 &copy; 2024 PRIMA VIP. All rights reserved.
             </div>
@@ -84,7 +84,7 @@ class AppServiceProvider extends ServiceProvider
 
         Filament::registerRenderHook(
             'panels::head.start',
-            static fn(): string => '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />',
+            static fn (): string => '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />',
         );
 
         Filament::registerRenderHook(PanelsRenderHook::HEAD_START, static function () {
@@ -107,16 +107,16 @@ class AppServiceProvider extends ServiceProvider
 
         FilamentView::registerRenderHook(
             'panels::head.start',
-            static fn(): string => '
+            static fn (): string => '
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
             <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap" rel="stylesheet">',
         );
 
         Filament::registerRenderHook(
-        // PanelsRenderHook::USER_MENU_PROFILE_AFTER,
-            PanelsRenderHook::SIDEBAR_NAV_END,
-            static fn() => view('partials.concierge-user-menu')
+            // PanelsRenderHook::USER_MENU_PROFILE_AFTER,
+            PanelsRenderHook::SIDEBAR_NAV_START,
+            static fn () => view('partials.concierge-user-menu')
         );
 
         Page::$reportValidationErrorUsing = static function (ValidationException $exception) {
