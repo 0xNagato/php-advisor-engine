0b8e67e0d9886266d259e2c0bf190ec597361b4e
Andrew Weir <andru.weir@gmail.com>
2024-04-30 23:26:16 -0400

    refactor: Add foreign keys to restaurants and schedule templates tables

---
+++ database/migrations/2024_05_01_020815_add_foreign_keys_to_restaurants_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('restaurants', static function (Blueprint $table) {
+            if (Schema::hasColumn('restaurants', 'user_id')) {
+                $table->dropForeign('restaurants_user_id_foreign');
+            }
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::table('schedule_templates', static function (Blueprint $table) {
+            if (Schema::hasColumn('schedule_templates', 'restaurant_id')) {
+                $table->dropForeign('schedule_templates_restaurant_id_foreign');
+            }
+            $table->foreign('restaurant_id')->references('id')->on('restaurants')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('restaurants', static function (Blueprint $table) {
+            $table->dropForeign('restaurants_user_id_foreign');
+        });
+
+        Schema::table('schedule_templates', static function (Blueprint $table) {
+            $table->dropForeign('schedule_templates_restaurant_id_foreign');
+        });
+    }
+};
