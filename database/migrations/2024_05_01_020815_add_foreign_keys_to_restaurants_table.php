c4f70027aa183656d5877ebce19d94f77c552875
Andrew Weir <andru.weir@gmail.com>
2024-04-30 23:50:37 -0400

    refactor: Add foreign keys to restaurants and schedule templates tables.
    Change: Add foreign keys 'restaurants_user_id_foreign' and 'schedule_templates_restaurant_id_foreign'
    Remove existing foreign keys if present

--- database/migrations/2024_05_01_020815_add_foreign_keys_to_restaurants_table.php
+++ database/migrations/2024_05_01_020815_add_foreign_keys_to_restaurants_table.php
@@ -4,22 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('restaurants', static function (Blueprint $table) {
-            if (Schema::hasColumn('restaurants', 'user_id')) {
+        Schema::table('restaurants', function (Blueprint $table) {
+            $connection = Schema::getConnection();
+            $dbSchemaManager = $connection->getDoctrineSchemaManager();
+            $doctrineTable = $dbSchemaManager->listTableDetails($table->getTable());
+
+            if ($doctrineTable->hasForeignKey('restaurants_user_id_foreign')) {
                 $table->dropForeign('restaurants_user_id_foreign');
             }
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
 
-        Schema::table('schedule_templates', static function (Blueprint $table) {
-            if (Schema::hasColumn('schedule_templates', 'restaurant_id')) {
+        Schema::table('schedule_templates', function (Blueprint $table) {
+            $connection = Schema::getConnection();
+            $dbSchemaManager = $connection->getDoctrineSchemaManager();
+            $doctrineTable = $dbSchemaManager->listTableDetails($table->getTable());
+
+            if ($doctrineTable->hasForeignKey('schedule_templates_restaurant_id_foreign')) {
                 $table->dropForeign('schedule_templates_restaurant_id_foreign');
             }
             $table->foreign('restaurant_id')->references('id')->on('restaurants')->onDelete('cascade');
