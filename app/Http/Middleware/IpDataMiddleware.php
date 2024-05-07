02c8119f1829c83301bab6779d970b7001c7d7ef
Andrew Weir <andru.weir@gmail.com>
2024-05-06 23:40:40 -0400

    Use local maxmind db to get ip info before reaching out for api

--- app/Http/Middleware/IpDataMiddleware.php
+++ app/Http/Middleware/IpDataMiddleware.php
@@ -4,13 +4,15 @@ namespace App\Http\Middleware;
 
 use App\Services\IPLocationService;
 use Closure;
+use Exception;
 use Illuminate\Http\Request;
+use Sentry;
 
 class IpDataMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        if (! $request->session()->has('region')) {
+        if (!$request->session()->has('region')) {
             if (in_array($request->ip(), ['127.0.0.1', '0.0.0.0'])) {
                 $request->session()->put('timezone', config('app.default_timezone'));
                 $request->session()->put('region', config('app.default_region'));
@@ -20,7 +22,13 @@ class IpDataMiddleware
 
             $ip = $request->ip();
 
-            $locationData = app(IPLocationService::class)->getLocationData($ip);
+            try {
+                $locationData = geoip()->getLocation($ip);
+            } catch (Exception $e) {
+                Sentry::captureException($e);
+                $locationData = app(IPLocationService::class)->getLocationData($ip);
+            }
+
             $region = app(IPLocationService::class)->getClosestRegion($locationData->lat, $locationData->lon);
 
             $request->session()->put('timezone', $region->timezone);
