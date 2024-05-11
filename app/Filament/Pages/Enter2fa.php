12114cc42a9f66c3ef178dfea0a7a785a3f89132
Arlind Musliu <arlindmusliu90@gmail.com>
2024-05-11 08:46:19 -0500

    custom page for 2FA

---
+++ app/Filament/Pages/Enter2fa.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Facades\Filament;
+use Filament\Pages\Page;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Actions\Concerns\InteractsWithActions;
+use Filament\Actions\Contracts\HasActions;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+
+class Enter2fa extends Page implements HasForms, HasActions
+{
+    use InteractsWithActions;
+    use InteractsWithForms;
+
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.enter2fa';
+
+    protected static ?string $title = 'Two Factor Authentication';
+
+    protected static bool $shouldRegisterNavigation = false;
+
+    public ?array $data = [];
+    public int $tries = 0;
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('code')
+                    ->numeric()
+                    ->length(6)
+                    ->required(),
+            ])->statePath('data');
+    }
+
+    public function mount()
+    {
+        // if this device is verified, redirect to the dashboard
+        if (session()->has('twofacode' . auth()->user()->id)) {
+            return redirect()->route('filament.admin.pages.admin-dashboard');
+        }
+
+        // hide the navigation for the 2FA page
+        Filament::getPanel()
+            ->navigation(false);
+
+        // send the code to the user
+        auth()->user()->generateCode();
+    }
+
+    public function save()
+    {
+        $code = $this->form->getState()['code'];
+
+        if (!auth()->user()->verify2FACode($code)) {
+            $this->tries++;
+
+            if ($this->tries >= 3) {
+                Filament::auth()->logout();
+                session()->invalidate();
+                session()->regenerateToken();
+
+                return redirect()->route('filament.admin.auth.login');
+            }
+
+            $this->addError('code', 'The provided 2FA code is incorrect.');
+            $this->reset('data');
+
+        } else {
+            auth()->user()->markDeviceAsVerified();
+            return redirect()->route('filament.admin.pages.admin-dashboard');
+        }
+    }
+
+    public function regenerateCode()
+    {
+        auth()->user()->generateCode();
+        $this->tries = 0;
+        $this->reset('data');
+    }
+}
