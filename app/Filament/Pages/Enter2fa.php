58a00185f55b388ddb2b40d9e766299b8ab80d20
arlindmusliu <arlindmusliu@users.noreply.github.com>
2024-05-13 09:03:52 +0000

    Fix styling

--- app/Filament/Pages/Enter2fa.php
+++ app/Filament/Pages/Enter2fa.php
@@ -41,7 +41,7 @@ class Enter2fa extends Page implements HasActions, HasForms
 
     public function mount()
     {
-        $sessionKey = 'twofacode' . auth()->id();
+        $sessionKey = 'twofacode'.auth()->id();
 
         // if this device is verified, redirect to the dashboard
         if (session()->has($sessionKey) && session($sessionKey) === true) {
--- app/Http/Middleware/TwoFactorAuthentication.php
+++ app/Http/Middleware/TwoFactorAuthentication.php
@@ -10,11 +10,11 @@ class TwoFactorAuthentication
 {
     public function handle(Request $request, Closure $next): Response
     {
-        if (!auth()->check()) {
+        if (! auth()->check()) {
             return $next($request);
         }
 
-        $sessionKey = 'twofacode' . auth()->id();
+        $sessionKey = 'twofacode'.auth()->id();
 
         if ($this->deviceIsVerified($sessionKey)) {
             return $next($request);
@@ -23,7 +23,7 @@ class TwoFactorAuthentication
         $device = auth()->user()->registerDevice();
         $this->storeDeviceVerificationStatus($sessionKey, $device->verified);
 
-        if (!$device->verified && $this->shouldRedirectTo2fa($request)) {
+        if (! $device->verified && $this->shouldRedirectTo2fa($request)) {
             return redirect()->route('filament.admin.pages.enter2fa');
         }
 
@@ -42,7 +42,7 @@ class TwoFactorAuthentication
 
     protected function shouldRedirectTo2fa(Request $request): bool
     {
-        return !$request->routeIs('filament.admin.pages.enter2fa') &&
-            !$request->routeIs('filament.admin.auth.logout');
+        return ! $request->routeIs('filament.admin.pages.enter2fa') &&
+            ! $request->routeIs('filament.admin.auth.logout');
     }
 }
