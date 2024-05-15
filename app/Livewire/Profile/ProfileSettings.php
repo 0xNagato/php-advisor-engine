b168a06ae20b05b64c4db9f3d1d24c0588cf33a3
Arlind Musliu <arlindmusliu90@gmail.com>
2024-05-15 09:11:41 -0500

    revert back changes

--- app/Livewire/Profile/ProfileSettings.php
+++ app/Livewire/Profile/ProfileSettings.php
@@ -12,7 +12,6 @@ use Filament\Notifications\Notification;
 use Filament\Widgets\Widget;
 use Illuminate\Support\Facades\Storage;
 use libphonenumber\PhoneNumberType;
-use Livewire\Component;
 use Tapp\FilamentTimezoneField\Forms\Components\TimezoneSelect;
 use Ysfkaya\FilamentPhoneInput\Forms\PhoneInput;
 
@@ -49,6 +48,7 @@ class ProfileSettings extends Widget implements HasForms
                 ->circleCropper()
                 ->visibility('public')
                 ->directory('profile-photos')
+                ->optimize('webp')
                 ->moveFiles()
                 ->hidden(function () {
                     return auth()->user()->hasRole('restaurant');
@@ -63,10 +63,6 @@ class ProfileSettings extends Widget implements HasForms
                 ->required()
                 ->label('Email')
                 ->unique('users', ignorable: auth()->user())
-                ->live(onBlur: true)
-                ->afterStateUpdated(function (Component $livewire) {
-                    $livewire->dispatch('open-modal', 'check2FACode');
-                })
                 ->email(),
             PhoneInput::make('phone')
                 ->required()
@@ -86,43 +82,17 @@ class ProfileSettings extends Widget implements HasForms
             ->statePath('data');
     }
 
-    public function check2FACode(): Action
-    {
-        return Action::make('check2FA')
-            ->label('Verify Email Change')
-            ->modalHeading('Verify Your Identity')
-            ->form([
-                TextInput::make('code')
-                    ->required()
-                    ->label('2FA Code'),
-            ])
-            ->action(function (array $data): void {
-                dd('2FA code is valid');
-                // Here you would check the 2FA code validity
-                // For example, compare it with the code stored in the database for the user
-                // If valid, update the email and other user details
-                // If not, return an error message
-            });
-    }
-
     public function save(): void
     {
         $data = $this->form->getState();
-        //check if the email field is modified
 
         if ($data['email'] !== auth()->user()->email) {
             $this->livewire->mountFormComponentAction('check2FACode');
             dd('2FA code is required');
         }
 
-        // Assuming the profile photo is stored as a file
         $profilePhotoPath = $data['profile_photo_path'];
-
-        // Make the profile photo file public
         Storage::disk('do')->setVisibility($profilePhotoPath, 'public');
-
-        // Update the user's profile photo file path
-        $data['profile_photo_path'] = $profilePhotoPath;
 
         auth()->user()->update($data);
 
