f20baff2685ab78cf1e6be31e7bbfec35fa61c24
Andrew Weir <andru.weir@gmail.com>
2024-05-26 22:28:52 -0400

    Update Restaurant Stats Styling

--- resources/views/livewire/restaurant/restaurant-stats.blade.php
+++ resources/views/livewire/restaurant/restaurant-stats.blade.php
@@ -1,11 +1,12 @@
 <x-filament-widgets::widget>
     <div class="sm:mt-0">
-        <dl class="grid divide-gray-200 overflow-hidden rounded-lg bg-white shadow grid-cols-3 divide-x divide-y-0">
+        <dl class="grid divide-x divide-gray-200 overflow-hidden rounded-lg bg-white shadow"
+            style="grid-template-columns: 20% 40% 40%;">
             <!-- Bookings -->
             <div class="px-4 py-5 sm:p-6">
                 <dt class="text-xs font-semibold text-slate-900">Bookings</dt>
                 <dd class="mt-1 flex items-baseline justify-between md:block lg:flex">
-                    <div class="flex flex-col items-baseline text-2xl font-semibold text-indigo-600">
+                    <div class="flex flex-col items-baseline text-xl font-semibold text-indigo-600">
                         {{ $stats->formatted['number_of_bookings'] }}
                         <div
                             @class(['flex items-center', 'text-[11px]', 'text-green-600' => $stats->difference['number_of_bookings_up'], 'text-red-600' => !$stats->difference['number_of_bookings_up']])>
@@ -25,7 +26,7 @@
             <div class="px-4 py-5 sm:p-6">
                 <dt class="text-xs font-semibold text-slate-900">PRIME Earnings</dt>
                 <dd class="mt-1 flex items-baseline justify-between md:block lg:flex">
-                    <div class="flex flex-col items-baseline text-2xl font-semibold text-indigo-600">
+                    <div class="flex flex-col items-baseline text-xl font-semibold text-indigo-600">
                         <div>
                             {{ $stats->formatted['restaurant_earnings'] }}
                         </div>
@@ -45,9 +46,9 @@
 
             <!-- Bounty -->
             <div class="px-4 py-5 sm:p-6">
-                <dt class="text-xs font-semibold text-slate-900">Marketing Investment</dt>
+                <dt class="text-xs font-semibold text-slate-900">Mktg. Investment</dt>
                 <dd class="mt-1 flex items-baseline justify-between md:block lg:flex">
-                    <div class="flex flex-col items-baseline text-2xl font-semibold text-indigo-600">
+                    <div class="flex flex-col items-baseline text-xl font-semibold text-indigo-600">
                         <div>
                             {{ $stats->formatted['restaurant_bounty'] }}
                         </div>
@@ -56,9 +57,9 @@
                             <div>{{ money($stats->difference['restaurant_bounty'], $currency) }}</div>
 
                             @if ($stats->difference['restaurant_bounty_up'])
-                                <x-filament::icon icon="heroicon-o-arrow-down-right" class="w-4 h-4" />
-                            @else
                                 <x-filament::icon icon="heroicon-o-arrow-up-right" class="w-4 h-4" />
+                            @else
+                                <x-filament::icon icon="heroicon-o-arrow-down-right" class="w-4 h-4" />
                             @endif
                         </div>
                     </div>
