3ac32ccb58784dbb8aaf52ee23c7d407ed3427e1
Arlind Musliu <arlindmusliu90@gmail.com>
2024-07-16 09:35:31 -0500

    testing a route with a region

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,26 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/{region}/test', function ($region) {
+        $data = [
+            'region' => $region,
+            'bookings' => [
+                [
+                    'id' => 1,
+                    'name' => 'Booking 1',
+                    'date' => '2021-01-01',
+                ],
+                [
+                    'id' => 2,
+                    'name' => 'Booking 2',
+                    'date' => '2021-01-02',
+                ],
+            ],
+        ];
+
+        return response()->json($data);
+    });
+});
