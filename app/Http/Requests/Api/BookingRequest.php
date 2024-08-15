df0b25e12d6466fc1062d0b88a5b20f3aee3e358
Arlind Musliu <arlindmusliu90@gmail.com>
2024-08-15 14:08:55 +0200

    Refactor validation requests (#62)
    * refactored timeslot request
    
    * date should be today or in the future
    
    * refactored region form requests
    
    * refactored av. calendar form requests
    
    * refactored hub form requests
    
    * API: Create Booking (#58)
    
    * booking process requirements
    
    * new api routes for BookingController
    
    * creating a pending booking
    
    * cancel a pending booking
    
    * fix booking time
    
    * returned prime_time value
    
    * renamed response message
    
    * modify reservation slots for hub
    
    * fixed return hours for hub
    
    * fixed wrong time for future dates
    
    * display seconds as a response for the hub
    
    * refactored the response to boolean
    
    * fixed timeslot response
    
    * fixed timeslot response
    
    * fixed timeslot response
    
    * refactored code
    
    * refactored booking form requests
    
    * refactored to Api folder
    
    * small change

---
+++ app/Http/Requests/Api/BookingRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class BookingRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'date' => ['required', 'date', 'after_or_equal:today'],
+            'schedule_template_id' => ['required', 'integer'],
+            'guest_count' => ['required', 'integer'],
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(
+            $validator->errors(), 422)
+        );
+    }
+}
---
+++ app/Http/Requests/Api/TimeslotRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class TimeslotRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'date' => ['required', 'date', 'after_or_equal:today'],
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(
+            $validator->errors(), 422)
+        );
+    }
+}
