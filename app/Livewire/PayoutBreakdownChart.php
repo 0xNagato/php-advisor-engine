7f3265dbf308686ba25c2106bb2cf163e7abdcb5
Andrew Weir <andru.weir@gmail.com>
2024-08-27 21:35:33 -0400

    Added pie chart for customer invoice

---
+++ app/Livewire/PayoutBreakdownChart.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Booking;
+use App\Models\Earning;
+use Filament\Support\RawJs;
+use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Str;
+
+class PayoutBreakdownChart extends ChartWidget
+{
+    public ?Booking $booking = null;
+
+    protected function getData(): array
+    {
+        if (! $this->booking) {
+            return [];
+        }
+
+        $earnings = Earning::with('user')
+            ->where('booking_id', $this->booking->id)
+            ->get();
+
+        $data = $earnings->map(fn ($earning) => [
+            'label' => $earning->user->name.' ('.Str::title(str_replace('_', ' ', $earning->type)).')',
+            'value' => $earning->amount,
+        ])->toArray();
+
+        $data[] = [
+            'label' => 'Platform',
+            'value' => $this->booking->platform_earnings,
+        ];
+
+        return [
+            'datasets' => [
+                [
+                    'data' => collect($data)->pluck('value')->toArray(),
+                    'backgroundColor' => [
+                        'rgba(79, 70, 229, 0.8)',   // indigo-600
+                        'rgba(249, 115, 22, 0.8)',  // orange-500
+                        'rgba(20, 184, 166, 0.8)',  // teal-500
+                        'rgba(244, 63, 94, 0.8)',   // rose-500
+                        'rgba(245, 158, 11, 0.8)',  // amber-500
+                        'rgba(34, 197, 94, 0.8)',   // green-500
+                        'rgba(16, 185, 129, 0.8)',  // green-600
+                        'rgba(100, 116, 139, 0.8)', // slate-500
+                        'rgba(71, 85, 105, 0.8)',   // slate-600
+                        'rgba(99, 102, 241, 0.8)',  // indigo-500
+                        'rgba(129, 140, 248, 0.8)', // indigo-400
+                        'rgba(251, 146, 60, 0.8)',  // orange-400
+                    ],
+                ],
+            ],
+            'labels' => collect($data)->pluck('label')->toArray(),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'pie';
+    }
+
+    protected function getOptions(): RawJs
+    {
+        $currency = $this->booking?->currency ?? 'USD';
+
+        return RawJs::make(<<<JS
+        {
+            scales: {
+                x: {
+                    display: false
+                },
+                y: {
+                    display: false
+                }
+            },
+            plugins: {
+                legend: {
+                    position: 'bottom'
+                },
+                tooltip: {
+                    callbacks: {
+                        label: function(context) {
+                            let label = context.label || '';
+                            if (label) {
+                                label += ': ';
+                            }
+                            const value = context.raw;
+                            if (value !== null) {
+                                const formattedValue = new Intl.NumberFormat('en-US', {
+                                    style: 'currency',
+                                    currency: '{$currency}',
+                                    minimumFractionDigits: 2
+                                }).format(value / 100);
+                                label += formattedValue;
+                            }
+                            return label;
+                        }
+                    }
+                }
+            }
+        }
+        JS);
+    }
+}
--- resources/views/livewire/customer-invoice.blade.php
+++ resources/views/livewire/customer-invoice.blade.php
@@ -1,4 +1,9 @@
-@php use libphonenumber\PhoneNumberFormat; @endphp
+@php
+    use App\Filament\Resources\ConciergeResource;
+    use App\Filament\Resources\PartnerResource;
+    use App\Filament\Resources\VenueResource;
+    use libphonenumber\PhoneNumberFormat;
+@endphp
 <div @class([
     'relative',
     'py-4' => !$download,
@@ -11,7 +16,7 @@
                 Email Invoice
             </x-filament::button>
             <x-filament::button color="indigo" class="w-1/2" size="sm" icon="gmdi-file-download-o" tag="a"
-                                :href="route('customer.invoice.download', ['uuid' => $booking->uuid])">
+                :href="route('customer.invoice.download', ['uuid' => $booking->uuid])">
                 Download PDF
             </x-filament::button>
         </div>
@@ -20,16 +25,15 @@
             <form wire:submit="emailInvoice" class="max-w-3xl p-4 mx-auto my-4 bg-gray-100 border rounded-lg">
                 {{ $this->form }}
                 <button type="submit"
-                        class="w-full px-4 py-2 mt-4 text-sm font-semibold text-white bg-indigo-600 rounded-lg">
+                    class="w-full px-4 py-2 mt-4 text-sm font-semibold text-white bg-indigo-600 rounded-lg">
                     Send Email
                 </button>
             </form>
         @endif
-
     @endif
 
     <div
-            class="bg-white rounded-xl shadow sm:max-w-3xl lg:mx-auto invoice-container flex flex-col @if ($download) min-h-[10in] @endif">
+        class="bg-white rounded-xl shadow sm:max-w-3xl lg:mx-auto invoice-container flex flex-col @if ($download) min-h-[10in] @endif">
         <div class="relative overflow-hidden bg-indigo-800 min-h-32 rounded-t-xl">
             @if (!isset($customerInvoice) && !$download)
                 <button class="p-4 font-semibold text-white" onclick="window.history.back();">
@@ -39,7 +43,7 @@
             <!-- SVG Background Element -->
             <figure class="absolute inset-x-0 bottom-0 -mb-px ">
                 <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                     viewBox="0 0 1920 100.1">
+                    viewBox="0 0 1920 100.1">
                     <path fill="currentColor" class="fill-white" d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"></path>
                 </svg>
             </figure>
@@ -49,13 +53,13 @@
         <div class="relative z-10 -mt-12">
             <!-- Icon -->
             <span
-                    class="mx-auto flex justify-center items-center size-[62px] rounded-full border border-gray-200 bg-white text-gray-700 shadow-sm dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400">
+                class="mx-auto flex justify-center items-center size-[62px] rounded-full border border-gray-200 bg-white text-gray-700 shadow-sm dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400">
                 <svg class="flex-shrink-0 size-6" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                     fill="currentColor" viewBox="0 0 16 16">
+                    fill="currentColor" viewBox="0 0 16 16">
                     <path
-                            d="M1.92.506a.5.5 0 0 1 .434.14L3 1.293l.646-.647a.5.5 0 0 1 .708 0L5 1.293l.646-.647a.5.5 0 0 1 .708 0L7 1.293l.646-.647a.5.5 0 0 1 .708 0L9 1.293l.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .801.13l.5 1A.5.5 0 0 1 15 2v12a.5.5 0 0 1-.053.224l-.5 1a.5.5 0 0 1-.8.13L13 14.707l-.646.647a.5.5 0 0 1-.708 0L11 14.707l-.646.647a.5.5 0 0 1-.708 0L9 14.707l-.646.647a.5.5 0 0 1-.708 0L7 14.707l-.646.647a.5.5 0 0 1-.708 0L5 14.707l-.646.647a.5.5 0 0 1-.708 0L3 14.707l-.646.647a.5.5 0 0 1-.801-.13l-.5-1A.5.5 0 0 1 1 14V2a.5.5 0 0 1 .053-.224l.5-1a.5.5 0 0 1 .367-.27zm.217 1.338L2 2.118v11.764l.137.274.51-.51a.5.5 0 0 1 .707 0l.646.647.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.509.509.137-.274V2.118l-.137-.274-.51.51a.5.5 0 0 1-.707 0L12 1.707l-.646.647a.5.5 0 0 1-.708 0L10 1.707l-.646.647a.5.5 0 0 1-.708 0L8 1.707l-.646.647a.5.5 0 0 1-.708 0L6 1.707l-.646.647a.5.5 0 0 1-.708 0L4 1.707l-.646.647a.5.5 0 0 1-.708 0l-.509-.51z"/>
+                        d="M1.92.506a.5.5 0 0 1 .434.14L3 1.293l.646-.647a.5.5 0 0 1 .708 0L5 1.293l.646-.647a.5.5 0 0 1 .708 0L7 1.293l.646-.647a.5.5 0 0 1 .708 0L9 1.293l.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .801.13l.5 1A.5.5 0 0 1 15 2v12a.5.5 0 0 1-.053.224l-.5 1a.5.5 0 0 1-.8.13L13 14.707l-.646.647a.5.5 0 0 1-.708 0L11 14.707l-.646.647a.5.5 0 0 1-.708 0L9 14.707l-.646.647a.5.5 0 0 1-.708 0L7 14.707l-.646.647a.5.5 0 0 1-.708 0L5 14.707l-.646.647a.5.5 0 0 1-.708 0L3 14.707l-.646.647a.5.5 0 0 1-.801-.13l-.5-1A.5.5 0 0 1 1 14V2a.5.5 0 0 1 .053-.224l.5-1a.5.5 0 0 1 .367-.27zm.217 1.338L2 2.118v11.764l.137.274.51-.51a.5.5 0 0 1 .707 0l.646.647.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.509.509.137-.274V2.118l-.137-.274-.51.51a.5.5 0 0 1-.707 0L12 1.707l-.646.647a.5.5 0 0 1-.708 0L10 1.707l-.646.647a.5.5 0 0 1-.708 0L8 1.707l-.646.647a.5.5 0 0 1-.708 0L6 1.707l-.646.647a.5.5 0 0 1-.708 0L4 1.707l-.646.647a.5.5 0 0 1-.708 0l-.509-.51z" />
                     <path
-                            d="M3 4.5a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm8-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z"/>
+                        d="M3 4.5a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm8-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z" />
                 </svg>
             </span>
             <!-- End Icon -->
@@ -78,10 +82,10 @@
                     <span class="block text-xs text-gray-500 uppercase">Customer:</span>
                     <div class="block text-sm font-medium text-gray-800 dark:text-gray-200">
                         {{ $booking->guest_name }}
-                        <br/>
+                        <br />
                         {{ $booking->local_formatted_guest_phone }}
                         @if ($booking->guest_email)
-                            <br/>
+                            <br />
                             {{ $booking->guest_email }}
                         @endif
                     </div>
@@ -106,26 +110,21 @@
                 <div>
                     <span class="block text-xs text-gray-500 uppercase">Payment Method:</span>
                     <div class="flex items-center gap-x-2">
-
-                        {{-- amex, diners, discover, eftpos_au, jcb, mastercard, unionpay, visa, or unknown --}}
-
                         @if ($booking->stripe_charge)
                             @if ($booking->stripe_charge->paymentMethodDetails->card->brand === 'visa')
-                                <x-fab-cc-visa class="w-6 h-6"/>
+                                <x-fab-cc-visa class="w-6 h-6" />
                             @endif
                             @if ($booking->stripe_charge->paymentMethodDetails->card->brand === 'mastercard')
-                                <x-fab-cc-mastercard class="w-6 h-6"/>
+                                <x-fab-cc-mastercard class="w-6 h-6" />
                             @endif
                             @if ($booking->stripe_charge->paymentMethodDetails->card->brand === 'amex')
-                                <x-fab-cc-amex class="w-6 h-6"/>
+                                <x-fab-cc-amex class="w-6 h-6" />
                             @endif
                             <span class="block text-sm font-medium text-gray-800 dark:text-gray-200">
                                 ••••{{ $booking->stripe_charge->paymentMethodDetails->card->last4 }}</span>
                         @else
                             <span class="block text-sm font-medium text-gray-800 dark:text-gray-200">Unknown</span>
-
                         @endif
-
                     </div>
                 </div>
                 <!-- End Col -->
@@ -142,7 +141,7 @@
 
                 <ul class="flex flex-col mt-3">
                     <li
-                            class="inline-flex items-center px-4 py-3 -mt-px text-sm text-gray-800 border gap-x-2 first:rounded-t-lg first:mt-0 last:rounded-b-lg dark:border-gray-700 dark:text-gray-200">
+                        class="inline-flex items-center px-4 py-3 -mt-px text-sm text-gray-800 border gap-x-2 first:mt-0 first:rounded-t-lg last:rounded-b-lg dark:border-gray-700 dark:text-gray-200">
                         <div class="flex items-center justify-between w-full">
                             <span>
                                 {{ $booking->venue->name }} ({{ $booking->guest_count }} guests)
@@ -154,7 +153,7 @@
                     </li>
                     @if ($booking->tax > 0)
                         <li
-                                class="inline-flex items-center px-4 py-3 -mt-px text-sm text-gray-800 border gap-x-2 first:rounded-t-lg first:mt-0 last:rounded-b-lg dark:border-gray-700 dark:text-gray-200">
+                            class="inline-flex items-center px-4 py-3 -mt-px text-sm text-gray-800 border gap-x-2 first:mt-0 first:rounded-t-lg last:rounded-b-lg dark:border-gray-700 dark:text-gray-200">
                             <div class="flex items-center justify-between w-full">
                                 <span>{{ $region->tax_rate_term }} ({{ $booking->tax * 100 }}%)</span>
                                 <span>
@@ -164,7 +163,7 @@
                         </li>
                     @endif
                     <li
-                            class="inline-flex items-center px-4 py-3 -mt-px text-sm font-semibold text-gray-800 border gap-x-2 bg-gray-50 first:rounded-t-lg first:mt-0 last:rounded-b-lg dark:bg-slate-800 dark:border-gray-700 dark:text-gray-200">
+                        class="inline-flex items-center px-4 py-3 -mt-px text-sm font-semibold text-gray-800 border gap-x-2 bg-gray-50 first:mt-0 first:rounded-t-lg last:rounded-b-lg dark:border-gray-700 dark:bg-slate-800 dark:text-gray-200">
                         <div class="flex items-center justify-between w-full">
                             <span>Amount Paid</span>
                             <span>
@@ -180,44 +179,94 @@
                     </div>
                 @endif
                 @if (auth()->check() && auth()->user()->hasRole('super_admin'))
-                    <x-filament::actions :actions="$this->resendInvoiceAction" class="w-full"/>
+                    <x-filament::actions :actions="$this->resendInvoiceAction" class="w-full" />
                 @endif
             </div>
 
             @if (auth()->check() && auth()->user()->hasRole('super_admin'))
-                <div class="mt-4">
-                    <div class="text-xs font-semibold capitalize">EARNINGS</div>
-                    <div class="grid grid-cols-3 gap-2 mt-2 text-xs">
-                        @foreach ($booking->earnings as $earning)
-                            <div class="truncate">{{ $earning->user->name }} ({{ $earning->user_id }})</div>
-                            <div>{{ \Illuminate\Support\Str::title(str_replace('_', ' ', $earning->type)) }}</div>
-                            <div class="text-right">
-                                {{ money($earning->amount, $booking->currency) }}
-                            </div>
-                        @endforeach
+                <div class="mt-8">
+                    <div class="flex flex-col gap-8 lg:flex-row">
+                        <div class="w-full lg:w-1/2">
+                            <div class="mb-4 text-xs font-semibold capitalize">Earnings Breakdown</div>
+                            <div class="grid grid-cols-3 gap-2 pb-4 mb-4 text-xs border-b">
+                                @foreach ($booking->earnings as $earning)
+                                    <div class="truncate">
+                                        @php
+                                            $earning->user->load('venue', 'concierge', 'partner');
+                                            $url = match ($earning->type) {
+                                                'venue' => VenueResource::getUrl('view', [
+                                                    'record' => $earning->user->venue->id,
+                                                ]),
+                                                'concierge_referral_1',
+                                                'concierge_referral_2',
+                                                'concierge'
+                                                    => ConciergeResource::getUrl('view', [
+                                                    'record' => $earning->user->concierge->id,
+                                                ]),
+                                                'partner',
+                                                'partner_concierge',
+                                                'partner_venue'
+                                                    => PartnerResource::getUrl('view', [
+                                                    'record' => $earning->user->partner->id,
+                                                ]),
+                                                default => null,
+                                            };
+                                        @endphp
+                                        @if ($url)
+                                            <a href="{{ $url }}" class="text-indigo-600 hover:underline">
+                                                {{ $earning->user->name }}
+                                            </a>
+                                        @else
+                                            {{ $earning->user->name }}
+                                        @endif
+                                    </div>
+                                    <div>
+                                        @php
+                                            $type = match ($earning->type) {
+                                                'concierge_referral_1' => 'Con. Ref. 1',
+                                                'concierge_referral_2' => 'Con. Ref. 2',
+                                                'partner_concierge' => 'Partner Con.',
+                                                'partner_venue' => 'Partner Venue',
+                                                default => \Illuminate\Support\Str::title($earning->type),
+                                            };
+                                        @endphp
+                                        {{ $type }}
+                                    </div>
+                                    <div class="text-right">
+                                        {{ money($earning->amount, $booking->currency) }}
+                                    </div>
+                                @endforeach
 
-                        @role('super_admin')
-                        <div class="mt-2">Platform:</div>
-                        <div class="col-span-2 mt-2 text-right">
-                            {{ money($booking->platform_earnings, $booking->currency) }}
-                        </div>
-                        @if ($booking->is_prime)
-                            <div class="mt-2 font-semibold">Total Payout:</div>
-                            <div class="col-span-2 mt-2 font-semibold text-right">
-                                {{ money($booking->earnings->sum('amount'), $booking->currency) }}
+                                <div class="col-span-2">Platform Earnings:</div>
+                                <div class="text-right">
+                                    {{ money($booking->platform_earnings, $booking->currency) }}
+                                </div>
                             </div>
-                        @endif
-                        @endrole
+
+                            <div class="grid grid-cols-3 gap-2 text-xs font-semibold">
+                                <div class="col-span-2">Total Payout:</div>
+                                <div class="text-right">
+                                    {{ money($booking->earnings->sum('amount'), $booking->currency) }}
+                                </div>
+
+                                <hr class="col-span-3 my-2 border-gray-300">
+
+                                <div class="col-span-2">Total Amount:</div>
+                                <div class="text-right">
+                                    {{ money($booking->total_with_tax_in_cents, $booking->currency) }}
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="w-full -mt-4 lg:w-1/2">
+                            <livewire:payout-breakdown-chart :booking="$booking" />
+                        </div>
                     </div>
                 </div>
             @endif
 
         </div>
         <!-- End Body -->
-
-        {{-- <div class="flex items-end justify-center flex-grow p-4 text-xs text-gray-500">
-
-        </div> --}}
     </div>
-    <x-filament-actions::modals/>
+    <x-filament-actions::modals />
 </div>
