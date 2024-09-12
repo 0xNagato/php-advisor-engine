f5b48d818e25d9b97f6fee7e471d92c6ee2664b9
Andrew Weir <andru.weir@gmail.com>
2024-09-12 07:26:00 -0400

    TelescopeAuthorize

--- app/Http/Middleware/TelescopeAuthorize.php
+++ app/Http/Middleware/TelescopeAuthorize.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Laravel\Telescope\Telescope;
 
@@ -10,6 +11,12 @@ class TelescopeAuthorize
     public function handle($request, $next)
     {
         Log::info('Telescope Authorize Middleware');
+
+        if (Auth::check() && Auth::user()?->hasRole('super_admin')) {
+            Log::info('Telescope Authorize Middleware - User is super admin');
+
+            return $next($request);
+        }
 
         return Telescope::check($request) ? $next($request) : abort(403);
     }
