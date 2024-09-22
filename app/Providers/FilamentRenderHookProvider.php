0cf9cb9c43d5387d00e3a11a52e6c092993f0191
Andrew Weir <andru.weir@gmail.com>
2024-09-22 07:43:07 -0400

    Add footer back to mobile pages

--- app/Providers/FilamentRenderHookProvider.php
+++ app/Providers/FilamentRenderHookProvider.php
@@ -57,16 +57,14 @@ class FilamentRenderHookProvider extends ServiceProvider
             static fn () => view('filament.admin.logo')
         );
 
-        if (! isPrimaApp()) {
-            FilamentView::registerRenderHook(
-                PanelsRenderHook::PAGE_END,
-                static fn () => new HtmlString('
+        FilamentView::registerRenderHook(
+            PanelsRenderHook::PAGE_END,
+            static fn () => new HtmlString('
                     <div class="mb-4 text-xs text-center">
                         &copy; 2024 PRIMA VIP. All rights reserved.
                     </div>
                 ')
-            );
-        }
+        );
 
         Filament::registerRenderHook(
             'panels::head.start',
