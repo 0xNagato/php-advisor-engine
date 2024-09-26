729999cbff9f29e6b5646381aaa49410c3e81e9f
Andrew Weir <andru.weir@gmail.com>
2024-09-25 20:42:17 -0400

    One more time

--- resources/views/components/loading-overlay.blade.php
+++ resources/views/components/loading-overlay.blade.php
@@ -3,11 +3,11 @@
         <div
             class="inline-block px-4 pt-5 pb-4 overflow-hidden text-left align-bottom transition-all transform sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
             <div>
-                <div class="flex items-center justify-center w-12 h-12 mx-auto bg-indigo-100 rounded-full">
+                <div class="flex items-center justify-center w-12 h-12 mx-auto bg-white rounded-full">
                     <x-filament::loading-indicator class="w-6 h-6 text-indigo-600" />
                 </div>
                 <div class="mt-3 text-center sm:mt-5">
-                    <h3 class="text-lg font-semibold text-indigo-700">
+                    <h3 class="text-lg font-semibold text-indigo-800">
                         Loading Data...
                     </h3>
                 </div>
