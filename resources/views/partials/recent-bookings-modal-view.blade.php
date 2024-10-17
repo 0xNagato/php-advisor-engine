c8bf45fbcb893e5b16fdf29e8ac576340ad61d3a
Julio Solis <js@juliosolis.com>
2024-10-17 12:24:55 -0600

    Add Modal for VIP Codes #102 (#105)
    * Add Modal for VIP Codes #102
    - Add booking count to modal view
    
    * Add Modal for VIP Codes #102
    - Add earnings to modal view

--- resources/views/partials/recent-bookings-modal-view.blade.php
+++ resources/views/partials/recent-bookings-modal-view.blade.php
@@ -7,6 +7,10 @@
         <dl class="grid grid-cols-2 text-xs sm:text-sm gap-x-4 gap-y-2">
             <dt class="font-semibold">Code:</dt>
             <dd>{{ $vipCode->code }}</dd>
+            <dt class="font-semibold">Bookings:</dt>
+            <dd>{{ $vipCode->bookings_count }}</dd>
+            <dt class="font-semibold">Earnings:</dt>
+            <dd>{{ money($vipCode->total_earnings_in_u_s_d* 100, 'USD') }}</dd>
             <dt class="font-semibold">Created at:</dt>
             <dd>{{ Carbon::parse($vipCode->created_at, auth()->user()->timezone)->format('M d, Y g:i A') }}</dd>
         </dl>
