5ac73d852b55ec7c826c5693bb8dd3686b953ad6
Andrew Weir <andru.weir@gmail.com>
2024-10-22 09:17:31 -0400

    Forgot to add new controller

---
+++ app/Http/Controllers/Api/AppConfigController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Cache\CacheManager;
+use Illuminate\Http\JsonResponse;
+
+class AppConfigController extends Controller
+{
+    private $cache;
+
+    public function __construct(CacheManager $cache)
+    {
+        $this->cache = $cache;
+    }
+
+    public function __invoke(): JsonResponse
+    {
+        return $this->cache->remember('app_config', 3600, function () {
+            return response()->json([
+                'bookings_enabled' => config('app.bookings_enabled'),
+                'bookings_disabled_message' => config('app.bookings_disabled_message'),
+                'login' => [
+                    'background_image' => config('app.login.background_image'),
+                    'text_color' => config('app.login.text_color'),
+                ],
+            ]);
+        });
+    }
+}
