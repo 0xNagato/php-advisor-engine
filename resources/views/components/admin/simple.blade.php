a257ff29a73757eba070d4c0ded2999343eb649f
Andrew Weir <andru.weir@gmail.com>
2025-01-14 15:41:09 -0500

    Update spacing for simple layout

--- resources/views/components/admin/simple.blade.php
+++ resources/views/components/admin/simple.blade.php
@@ -19,13 +19,13 @@
     <div class="w-full pt-6 text-3xl font-bold leading-5 tracking-tight text-center text-gray-950">
         PRIMA
     </div>
-    <div class="p-2 mb-4 text-2xl font-bold tracking-tight text-center dm-serif text-gray-950">
+    <div class="p-2 mb-1 text-2xl font-bold tracking-tight text-center dm-serif text-gray-950">
         Everybody Wins<span class="font-sans font-normal">™</span>
     </div>
 
     @if ($showWrapper)
         <div
-            class="bg-white px-6 pt-8 pb-8 shadow-sm ring-1 ring-gray-950/5 rounded-lg mx-4 {{ $containerSize }} sm:mx-auto">
+            class="bg-white px-6 pt-4 pb-8 shadow-sm ring-1 ring-gray-950/5 rounded-lg mx-4 {{ $containerSize }} sm:mx-auto">
     @endif
     <div class="mb-4 text-lg font-semibold text-center">
         {{ $this->getHeading() }}
