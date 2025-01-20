b2e1a498973fef3a3e91c07b8bc2ab3fdf9b7267
Andrew Weir <andru.weir@gmail.com>
2025-01-20 00:43:37 -0500

    Update labels on comic

--- resources/views/livewire/story.blade.php
+++ resources/views/livewire/story.blade.php
@@ -4,17 +4,14 @@
     <div
         class="fixed inset-x-0 bottom-0 z-50 flex items-center justify-center gap-4 p-3 text-xs font-medium text-indigo-600 shadow-lg bg-white/95 backdrop-blur-md whitespace-nowrap">
         <div class="flex items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
-            </svg>
-            <span>Swipe right to navigate</span>
+
+            <span>Swipe to navigate</span>
         </div>
 
         <div class="w-px h-4 bg-indigo-200"></div>
 
         <div class="flex items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 24 24" fill="none"
-                stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
