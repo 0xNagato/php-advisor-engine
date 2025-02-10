7a57b6250d8305b4901344130edd609904609211
Andrew Weir <andru.weir@gmail.com>
2025-02-10 12:25:09 -0500

    Fix android logo

--- resources/views/filament/pages/messages/list-messages.blade.php
+++ resources/views/filament/pages/messages/list-messages.blade.php
@@ -7,12 +7,13 @@
     @nonmobileapp
         @php
             $userAgent = Request::userAgent();
-            $isIosDevice = (str_contains($userAgent, 'iPhone') || str_contains($userAgent, 'iPad')) 
-                && !str_contains($userAgent, 'CriOS') 
-                && !str_contains($userAgent, 'FxiOS');
+            $isIosDevice =
+                (str_contains($userAgent, 'iPhone') || str_contains($userAgent, 'iPad')) &&
+                !str_contains($userAgent, 'CriOS') &&
+                !str_contains($userAgent, 'FxiOS');
             $isAndroidDevice = str_contains($userAgent, 'Android');
         @endphp
-        @if($isIosDevice)
+        @if ($isIosDevice)
             <div
                 class="flex flex-col items-center p-3 -my-4 text-xs border-2 border-indigo-600 rounded-lg sm:text-base bg-gray-50">
                 <div class="flex items-center mb-3">
@@ -28,13 +29,17 @@
                 </a>
             </div>
         @elseif($isAndroidDevice)
-            <div class="flex flex-col items-center p-3 -my-4 text-xs border-2 border-green-600 rounded-lg sm:text-base bg-gray-50">
+            <div
+                class="flex flex-col items-center p-3 -my-4 text-xs border-2 border-green-600 rounded-lg sm:text-base bg-gray-50">
                 <div class="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12 mr-4 text-green-600" viewBox="0 0 24 24" fill="currentColor">
-                        <path d="M6 18c0 .55.45 1 1 1h1v3.5c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5V19h2v3.5c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5V19h1c.55 0 1-.45 1-1V8H6v10zM3.5 8C2.67 8 2 8.67 2 9.5v7c0 .83.67 1.5 1.5 1.5S5 17.33 5 16.5v-7C5 8.67 4.33 8 3.5 8zm17 0c-.83 0-1.5.67-1.5 1.5v7c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5v-7c0-.83-.67-1.5-1.5-1.5zm-4.97-5.84l1.3-1.3c.2-.2.2-.51 0-.71-.2-.2-.51-.2-.71 0l-1.48 1.48C13.85 1.23 12.95 1 12 1c-.96 0-1.86.23-2.66.63L7.85.15c-.2-.2-.51-.2-.71 0-.2.2-.2.51 0 .71l1.31 1.31C6.97 3.26 6 5.01 6 7h12c0-1.99-.97-3.75-2.47-4.84zM10 5H9V4h1v1zm5 0h-1V4h1v1z"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="flex-shrink-0 w-12 h-12 mr-4 text-green-600"
+                        viewBox="0 0 24 24" fill="currentColor">
+                        <path
+                            d="M6 18c0 .55.45 1 1 1h1v3.5c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5V19h2v3.5c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5V19h1c.55 0 1-.45 1-1V8H6v10zM3.5 8C2.67 8 2 8.67 2 9.5v7c0 .83.67 1.5 1.5 1.5S5 17.33 5 16.5v-7C5 8.67 4.33 8 3.5 8zm17 0c-.83 0-1.5.67-1.5 1.5v7c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5v-7c0-.83-.67-1.5-1.5-1.5zm-4.97-5.84l1.3-1.3c.2-.2.2-.51 0-.71-.2-.2-.51-.2-.71 0l-1.48 1.48C13.85 1.23 12.95 1 12 1c-.96 0-1.86.23-2.66.63L7.85.15c-.2-.2-.51-.2-.71 0-.2.2-.2.51 0 .71l1.31 1.31C6.97 3.26 6 5.01 6 7h12c0-1.99-.97-3.75-2.47-4.84zM10 5H9V4h1v1zm5 0h-1V4h1v1z" />
                     </svg>
                     <div>
-                        The PRIMA App for Android devices is currently in final stages of testing. Please use the web version of PRIMA for now. Thank you for your patience.
+                        The PRIMA App for Android devices is currently in final stages of testing. Please use the web
+                        version of PRIMA for now. Thank you for your patience.
                     </div>
                 </div>
             </div>
