7771906612356cee30db50d01e4225ba6b57433a
Andrew Weir <andru.weir@gmail.com>
2025-03-08 07:07:48 -0500

    Ability to mark booking as confirmed by venue

--- app/Livewire/Admin/ConfirmationManagerBookings.php
+++ app/Livewire/Admin/ConfirmationManagerBookings.php
@@ -36,16 +36,28 @@ class ConfirmationManagerBookings extends BaseWidget
             ->actions([
                 ActionGroup::make([
                     Action::make('resendConfirmation')
-                        ->hidden(fn (Booking $record) => ! auth()->user()->hasActiveRole('super_admin'))
+                        ->hidden(fn (Booking $record) => ! auth()->user()->hasActiveRole('super_admin') || ! is_null($record->venue_confirmed_at))
                         ->label('Resend Confirmation')
                         ->requiresConfirmation()
                         ->icon(fn (Booking $record) => is_null($record->venue_confirmed_at) ? 'ri-refresh-line' : 'heroicon-o-check-circle')
                         ->color(fn (Booking $record) => is_null($record->venue_confirmed_at) ? 'indigo' : 'success')
                         ->requiresConfirmation()
-                        ->hidden(fn (Booking $record) => ! is_null($record->venue_confirmed_at))
                         ->action(function (Booking $record) {
                             SendConfirmationToVenueContacts::run(booking: $record, reminder: true);
                             $record->update(['resent_venue_confirmation_at' => now()]);
+                        }),
+                    Action::make('markAsConfirmed')
+                        ->hidden(fn (Booking $record) => ! auth()->user()->hasActiveRole('super_admin') || ! is_null($record->venue_confirmed_at))
+                        ->label('Mark as Confirmed')
+                        ->requiresConfirmation()
+                        ->icon('heroicon-o-check-badge')
+                        ->color('success')
+                        ->action(function (Booking $record) {
+                            $record->update(['venue_confirmed_at' => now()]);
+                            \Filament\Notifications\Notification::make()
+                                ->title('Booking marked as confirmed')
+                                ->success()
+                                ->send();
                         }),
                 ]),
             ]);
