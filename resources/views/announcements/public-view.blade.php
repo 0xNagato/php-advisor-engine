eb3abe294de80c39964d08351a112781a4d6e5ae
Andrew Weir <andru.weir@gmail.com>
2025-05-08 00:53:43 -0400

    Update platform view link styling to match login button

--- resources/views/announcements/public-view.blade.php
+++ resources/views/announcements/public-view.blade.php
@@ -34,11 +34,9 @@
                         
                         @if(isset($showPlatformLink) && $showPlatformLink)
                             <div class="mt-8 border-t pt-6">
-                                <a href="{{ $platformUrl }}" class="inline-flex items-center gap-x-2 text-indigo-600 hover:text-indigo-800 font-medium">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd" />
-                                    </svg>
-                                    View in Platform
+                                <p class="mb-4 text-sm">View this announcement in the platform for full features.</p>
+                                <a href="{{ $platformUrl }}" class="inline-flex items-center justify-center w-full md:w-auto px-6 py-3 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 no-underline hover:no-underline !decoration-transparent" style="text-decoration: none !important;">
+                                    <span class="text-white">View in Platform</span>
                                 </a>
                             </div>
                         @endif
