26961cf4e2a1189e1fcd856fb880688382fe7568
Andrew Weir <andru.weir@gmail.com>
2025-05-08 00:52:39 -0400

    Stop redirecting logged-in users from public announcements
    - Keep logged-in users on the same URL when viewing public announcements
    - Add 'View in Platform' link for logged-in users instead of automatic redirect
    - Continue marking messages as read when appropriate

--- app/Http/Controllers/PublicAnnouncementController.php
+++ app/Http/Controllers/PublicAnnouncementController.php
@@ -8,24 +8,28 @@ use Illuminate\View\View;
 
 class PublicAnnouncementController extends Controller
 {
-    public function show(int $messageId): View|RedirectResponse
+    public function show(int $messageId): View
     {
         $message = Message::with('announcement')->findOrFail($messageId);
-
-        // If the user is logged in, redirect them to the platform view
+        $showPlatformLink = false;
+        
+        // If the user is logged in, mark the message as read if appropriate
+        // and provide a link to platform view instead of redirecting
         if (auth()->check()) {
             // Mark as read if the user is the recipient
             if ($message->user_id === auth()->id() && is_null($message->read_at)) {
                 $message->update(['read_at' => now()]);
             }
-
-            // Redirect to the internal platform view
-            return redirect()->route('filament.admin.resources.messages.view', ['record' => $messageId]);
+            
+            // Set flag to show platform link
+            $showPlatformLink = true;
         }
 
-        // Otherwise show the public view
+        // Show the public view for all users with appropriate links
         return view('announcements.public-view', [
             'message' => $message,
+            'showPlatformLink' => $showPlatformLink,
+            'platformUrl' => route('filament.admin.resources.messages.view', ['record' => $messageId]),
         ]);
     }
 }
--- resources/views/announcements/public-view.blade.php
+++ resources/views/announcements/public-view.blade.php
@@ -31,6 +31,17 @@
                                 </a>
                             </div>
                         @endguest
+                        
+                        @if(isset($showPlatformLink) && $showPlatformLink)
+                            <div class="mt-8 border-t pt-6">
+                                <a href="{{ $platformUrl }}" class="inline-flex items-center gap-x-2 text-indigo-600 hover:text-indigo-800 font-medium">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd" />
+                                    </svg>
+                                    View in Platform
+                                </a>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
