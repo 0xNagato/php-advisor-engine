4239adf8cbbff6d896c808fc015301f4e129ec60
Andrew Weir <andru.weir@gmail.com>
2025-05-11 00:08:19 -0400

    Add printable QRCode

---
+++ app/Http/Controllers/VipCodeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\VipCode;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\View\View;
+
+class VipCodeController extends Controller
+{
+    /**
+     * Display a printable version of a VIP code QR code
+     */
+    public function printQRCode(Request $request): View
+    {
+        $vipCode = VipCode::where('code', $request->input('code'))->firstOrFail();
+        $templateUrl = asset('images/printer_template.png');
+
+        // The SVG path will be passed directly to the view
+        $svgPath = $request->input('svg_path');
+
+        if (! $svgPath || ! Storage::disk('public')->exists($svgPath)) {
+            abort(404, 'QR code image not found');
+        }
+
+        $qrUrl = asset('storage/'.$svgPath);
+
+        return view('vip-code.print', [
+            'code' => $vipCode->code,
+            'qrUrl' => $qrUrl,
+            'templateUrl' => $templateUrl,
+        ]);
+    }
+}
